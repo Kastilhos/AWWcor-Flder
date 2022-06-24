35b1c21b9fccd8c42d2ec63e8655910188fe758d
Kastilhos <g.kastilhos@gmail.com>
2022-06-24 02:31:30 -0300

    Makeshift JS filter

--- src/index.js
+++ src/index.js
@@ -1,1 +1,80 @@
-console.log("Hi");
+const filterAll = document.getElementById("all")
+const filterArtwork = document.getElementById("artwork")
+const filterWebDesign = document.getElementById("web-design")
+const filterGraphDesign = document.getElementById("graph-design")
+const artwork = document.getElementsByClassName("portfolio__artwork")
+const webDesign = document.getElementsByClassName("portfolio__web-design")
+const graphDesign = document.getElementsByClassName("portfolio__graph-design")
+
+/*
+filterAll.addEventListener('click', function() {
+    if ( document.getElementById("All").classList.contains('portfolio__filter--off') )
+})
+*/
+filterArtwork.addEventListener('click', function () {
+    if (filterAll.classList.contains("portfolio__filter--on")) {
+        filterAll.classList.toggle('portfolio__filter--on')
+        this.classList.toggle('portfolio__filter--on')
+        for (let i of artwork) {
+            for (let i of graphDesign) {
+                i.classList.toggle('portfolio__graph-design--off')
+            }
+            for (let i of webDesign) {
+                i.classList.toggle('portfolio__web-design--off')
+            }
+        }
+    } else {
+        this.classList.toggle('portfolio__filter--on')
+        for (let i of artwork) {
+            for (let i of graphDesign) {
+                i.classList.toggle('portfolio__graph-design--off')
+            }
+            for (let i of webDesign) {
+                i.classList.toggle('portfolio__web-design--off')
+            }
+        }
+    }
+}
+)
+
+filterWebDesign.addEventListener('click', function () {
+    if (filterAll.classList.contains("portfolio__filter--on")) {
+        filterAll.classList.toggle('portfolio__filter--on')
+        this.classList.toggle('portfolio__filter--on')
+        for (let i of artwork) {
+            i.classList.toggle('portfolio__artwork--off')
+            }
+        for (let i of graphDesign) {
+            i.classList.toggle('portfolio__graph-design--off')
+            }
+    } else {
+        this.classList.toggle('portfolio__filter--on')
+        for (let i of artwork) {
+            i.classList.toggle('portfolio__artwork--off')
+            }
+        for (let i of graphDesign) {
+            i.classList.toggle('portfolio__graph-design--off')
+            }
+    }
+})
+
+filterGraphDesign.addEventListener('click', function () {
+    if (filterAll.classList.contains("portfolio__filter--on")) {
+        filterAll.classList.toggle('portfolio__filter--on')
+        this.classList.toggle('portfolio__filter--on')
+        for (let i of artwork) {
+            i.classList.toggle('portfolio__artwork--off')
+            }
+        for (let i of webDesign) {
+            i.classList.toggle('portfolio__web-design--off')
+        }
+    } else {
+        this.classList.toggle('portfolio__filter--on') 
+        for (let i of artwork) {
+            i.classList.toggle('portfolio__artwork--off')
+            }
+        for (let i of webDesign) {
+            i.classList.toggle('portfolio__web-design--off')
+        }
+    }
+})
