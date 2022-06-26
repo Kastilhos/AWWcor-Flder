b293fe6f71248e3cfba8830df8b71aba08867456
Kastilhos <g.kastilhos@gmail.com>
2022-06-26 15:19:53 -0300

    Individual filters done, fixing all

--- src/index.js
+++ src/index.js
@@ -6,37 +6,91 @@ const artwork = document.getElementsByClassName("portfolio__artwork")
 const webDesign = document.getElementsByClassName("portfolio__web-design")
 const graphDesign = document.getElementsByClassName("portfolio__graph-design")
 
-/*
+console.log(filterAll.classList.contains('portfolio__filter--on'))
+
+
 filterAll.addEventListener('click', function() {
-    if ( document.getElementById("All").classList.contains('portfolio__filter--off') )
+    if ( filterAll.classList.contains('portfolio__filter--on') == false ) {
+        this.classList.toggle('portfolio__filter--on')
+
+        if (filterGraphDesign.classList.contains('portfolio__filter--on')) {
+            for (let i of graphDesign) {
+                i.classList.toggle('portfolio__graph-design--off')
+            }
+        }
+
+        if (filterWebDesign.classList.contains('portfolio__filter--on')) {
+            for (let i of webDesign) {
+                i.classList.toggle('portfolio__web-design--off')
+            }
+        }
+
+        if (filterArtwork.classList.contains('portfolio__filter--on')) {
+            for (let i of artwork) {
+                i.classList.toggle('portfolio__artwork--off')
+            }
+        }
+    }
 })
-*/
+
 filterArtwork.addEventListener('click', function () {
     if (filterAll.classList.contains("portfolio__filter--on")) {
         filterAll.classList.toggle('portfolio__filter--on')
         this.classList.toggle('portfolio__filter--on')
-        for (let i of artwork) {
-            for (let i of graphDesign) {
-                i.classList.toggle('portfolio__graph-design--off')
-            }
-            for (let i of webDesign) {
-                i.classList.toggle('portfolio__web-design--off')
-            }
+        for (let i of graphDesign) {
+            i.classList.toggle('portfolio__graph-design--off')
+        }
+        for (let i of webDesign) {
+            i.classList.toggle('portfolio__web-design--off')
         }
     } else {
         this.classList.toggle('portfolio__filter--on')
         for (let i of artwork) {
-            for (let i of graphDesign) {
-                i.classList.toggle('portfolio__graph-design--off')
+                i.classList.toggle('portfolio__artwork--off')
             }
-            for (let i of webDesign) {
+        }
+    }
+)
+
+filterWebDesign.addEventListener('click', function () {
+    if (filterAll.classList.contains("portfolio__filter--on")) {
+        filterAll.classList.toggle('portfolio__filter--on')
+        this.classList.toggle('portfolio__filter--on')
+        for (let i of graphDesign) {
+            i.classList.toggle('portfolio__graph-design--off')
+        }
+        for (let i of artwork) {
+            i.classList.toggle('portfolio__artwork--off')
+        }
+    } else {
+        this.classList.toggle('portfolio__filter--on')
+        for (let i of webDesign) {
                 i.classList.toggle('portfolio__web-design--off')
             }
         }
     }
-}
 )
 
+filterGraphDesign.addEventListener('click', function () {
+    if (filterAll.classList.contains("portfolio__filter--on")) {
+        filterAll.classList.toggle('portfolio__filter--on')
+        this.classList.toggle('portfolio__filter--on')
+        for (let i of graphDesign) {
+            i.classList.toggle('portfolio__graph-design--off')
+        }
+        for (let i of artwork) {
+            i.classList.toggle('portfolio__web-design--off')
+        }
+    } else {
+        this.classList.toggle('portfolio__filter--on')
+        for (let i of graphDesign) {
+                i.classList.toggle('portfolio__graph-design--off')
+            }
+        }
+    }
+)
+
+/*
 filterWebDesign.addEventListener('click', function () {
     if (filterAll.classList.contains("portfolio__filter--on")) {
         filterAll.classList.toggle('portfolio__filter--on')
@@ -78,3 +132,4 @@ filterGraphDesign.addEventListener('click', function () {
         }
     }
 })
+*/
