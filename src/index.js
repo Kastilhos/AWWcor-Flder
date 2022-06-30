7a273d22042e4ff9a88694695f2aa3b1e14cf266
Kastilhos <g.kastilhos@gmail.com>
2022-06-30 02:36:51 -0300

    Finishing DRY (I hope so) filter - There's a bug

--- src/index.js
+++ src/index.js
@@ -1,86 +1,79 @@
-    const filter = {
-        all: document.getElementById("all"),
-        button: {
-            'web-design': document.getElementById("web-design"),
-            'graph-design': document.getElementById("graph-design"),
-            'artwork': document.getElementById("artwork")
-        },
-        display: {
-            'web-design': document.getElementsByClassName("portfolio__web-design"),
-            'graph-design': document.getElementsByClassName("portfolio__graph-design"),
-            'artwork': document.getElementsByClassName("portfolio__artwork")
-        }
+// Why addEventListener... We will explain later
+
+document.getElementById("all").addEventListener('click', () => {
+    filterButton('all');
+});
+document.getElementById("web-design").addEventListener('click', () => {
+    filterButton('web-design');
+});
+document.getElementById("graphic-design").addEventListener('click', () => {
+    filterButton('graphic-design');
+});
+document.getElementById("artwork").addEventListener('click', () => {
+    filterButton('artwork');
+});
+
+
+const filter = {
+    'all': document.getElementById("all"),
+    button: {
+        'web-design': document.getElementById("web-design"),
+        'graphic-design': document.getElementById("graphic-design"),
+        'artwork': document.getElementById("artwork")
+    },
+    display: {
+        'web-design': document.getElementsByClassName("portfolio__web-design"),
+        'graphic-design': document.getElementsByClassName("portfolio__graphic-design"),
+        'artwork': document.getElementsByClassName("portfolio__artwork")
     }
+}
 
 function filterButton(select) {
-    toggleButton = (selected) => {
-        filter.button[selected].toggle('portfolio__filter--toggled')
-    }/*
-    toggleFilter = (selected) => {
-        for (let i of filter.display.selected)
-            i.classList.toggle(`portfolio__${i}--toggled`)
-    }*/
+    
+    const toggleButton = (selected) => {
 
-    if (select == "all" && !filter.all.contains("portfolio__filter--toggled")) {
-        for (let i in filter.button) {
-            if (i.contains("portfolio__filter--toggled")) {
-                toggleButton(i);
-            }
-        }/*
-        for (let i of filter.display) {
-            toggleFilter(i);
-        }*/
-    }
-}
-
-let a = filter.all.addEventListener('click',filterButton("all"));
-let b = filter.button["web-design"].addEventListener('click',filterButton("web-design"));
-
-filterButton(all);
-
-
-
-
-
-
-
-
-
-
-/*
-    function toggleSingle(selected) {
-
-        portfolioFilter[selected].button.classList.toggle('portfolio__filter--toggled');
-        for (let i in portfolioFilter) {
-            for (let j of portfolioFilter[i].display) {
-                    j.classList.toggle(`portfolio__${i}--toggled`);
-            }
+        if (selected === "all") {
+            filter[selected].classList.toggle('portfolio__filter--toggled')
+        } else {
+            console.log(selected)
+            filter.button[selected].classList.toggle('portfolio__filter--toggled')
         }
+    }
+    
+    const toggleFilter = (selected) => {
 
+        for (let img of filter.display[selected]) {
+            img.classList.toggle(`portfolio__${selected}--toggled`)
+        }
     }
 
-    function toggleAll(selected) {
-        console.log(select)
-        portfolioFilter[selected].button.classList.toggle('portfolio__filter--toggled');
-
-        for (let i in portfolioFilter) {
-            console.log(i)
-            console.log(i != selected)
-            if (i != selected && portfolioFilter[i].button.classList.contains('portfolio__filter--toggled')) {
-                console.log(portfolioFilter[i].button)
-                portfolioFilter[i].button.classList.toggle('portfolio__filter--toggled');
+    const toggleAll = (selected) => {
+        
+        for (let button in filter.button) {
+            if (this[button].classList.contains("portfolio__filter--toggled")) {
+                toggleButton(button);
             } else {
-                for (let j of portfolioFilter[i].display) {
-                    j.classList.toggle(`portfolio__${i}--toggled`);
-                }
+                toggleFilter(button);
             }
-        };
+            toggleButton(selected);
+        }
     }
 
-    //For All
-    if (!portfolioFilter[select].button.classList.contains('portfolio__filter--toggled')) {
-        let a = select;
-        return toggleAll(a);
-    };
+    if (select === "all" && filter[select].classList.contains("portfolio__filter--toggled")) {
+        return;
+    } else if (select === "all" && !filter[select].classList.contains("portfolio__filter--toggled")) { 
+        toggleAll(select);
+    } else if (filter['all'].classList.contains("portfolio__filter--toggled")) {
+        toggleButton("all");
+        toggleButton(select);
+        toggleAll(select);
+    } else {
+        toggleButton(select);
+        if (/*every button.key that does not contain toggle*/false) {
+            toggleAll("all")
+        } else {
+            toggleFilter(select)
+        }
+    }
+
 }
-*/
